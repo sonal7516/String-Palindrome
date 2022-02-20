7fe94ac6b1e0c23855e873ddf702e309932510ae
sonal7516 <singhsonalb101096@gmail.com>
2022-02-20 20:15:03 +0530

    Add project files.

---
+++ loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+             Array.Reverse(arr);
+            string str_reverse = new string(arr);
+
+            if (str.ToLower() == str_reverse.ToLower())
+            {
+                Console.WriteLine("Given string is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Given string is not a palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
